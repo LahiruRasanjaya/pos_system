7fb9ac43907c2b9e37465e85a028200ab0bd0733
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2023-10-17 19:35:04 +0530

    Add ItemDataAccess

--- src/main/java/lk/ijse/dep11/pos/controller/ManageItemFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/ManageItemFormController.java
@@ -7,13 +7,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep11.pos.db.ItemDataAccess;
+import lk.ijse.dep11.pos.tm.Item;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 
 public class ManageItemFormController {
     public AnchorPane root;
@@ -22,7 +27,7 @@ public class ManageItemFormController {
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
     public JFXButton btnDelete;
-    public TableView tblItems;
+    public TableView<Item> tblItems;
     public JFXTextField txtUnitPrice;
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
@@ -33,6 +38,36 @@ public class ManageItemFormController {
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
         Platform.runLater(primaryStage::sizeToScene);
+    }
+    public void initialize(){
+        tblItems.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblItems.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblItems.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qty"));
+        tblItems.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        btnDelete.setDisable(true);
+        btnSave.setDefaultButton(true);
+        try {
+            tblItems.getItems().addAll(ItemDataAccess.getAllItems());
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to load items, try later").show();
+            e.printStackTrace();
+        }
+        Platform.runLater(txtCode::requestFocus);
+        tblItems.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur)->{
+            if (cur == null){
+                btnSave.setText("SAVE");
+                btnDelete.setDisable(true);
+                txtCode.setDisable(false);
+            }else{
+                btnSave.setText("UPDATE");
+                btnDelete.setDisable(false);
+                txtCode.setText(cur.getCode());
+                txtCode.setDisable(true);
+                txtDescription.setText(cur.getDescription());
+                txtQtyOnHand.setText(cur.getQty()  + "");
+                txtUnitPrice.setText(cur.getUnitPrice() + "");
+            }
+        });
     }
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
