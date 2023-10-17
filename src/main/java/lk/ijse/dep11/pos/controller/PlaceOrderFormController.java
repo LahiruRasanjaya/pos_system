1b79f1b4893235f9508e335257245935ff6f2cc6
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2023-10-17 22:43:48 +0530

    PlaceOrderFormController initialize

--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
@@ -10,12 +10,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep11.pos.tm.Customer;
+import lk.ijse.dep11.pos.tm.Item;
+import lk.ijse.dep11.pos.tm.OrderItem;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class PlaceOrderFormController {
     public AnchorPane root;
@@ -23,24 +30,78 @@ public class PlaceOrderFormController {
     public JFXTextField txtDescription;
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
-    public TableView tblOrderDetails;
+    public TableView<OrderItem> tblOrderDetails;
     public JFXTextField txtUnitPrice;
-    public JFXComboBox cmbCustomerId;
-    public JFXComboBox cmbItemCode;
+    public JFXComboBox<Customer> cmbCustomerId;
+    public JFXComboBox <Item>cmbItemCode;
     public JFXTextField txtQty;
     public Label lblId;
     public Label lblDate;
     public Label lblTotal;
     public JFXButton btnPlaceOrder;
+    public void initialize() throws IOException {
+        String[] cols = {"code", "description", "qty", "unitPrice", "total", "btnDelete"};
+        for (int i = 0; i < cols.length; i++) {
+            tblOrderDetails.getColumns().get(i).setCellValueFactory(new PropertyValueFactory<>(cols[i]));
+        }
+
+        lblDate.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        newOrder();
+        cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur) -> {
+            enablePlaceOrderButton();
+            if (cur != null) {
+                txtCustomerName.setText(cur.getName());
+                txtCustomerName.setDisable(false);
+                txtCustomerName.setEditable(false);
+            } else {
+                txtCustomerName.clear();
+                txtCustomerName.setDisable(true);
+            }
+        });
+        cmbItemCode.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur) -> {
+            if (cur != null) {
+                txtDescription.setText(cur.getDescription());
+                txtQtyOnHand.setText(cur.getQty() + "");
+                txtUnitPrice.setText(cur.getUnitPrice().toString());
+
+                for (TextField txt : new TextField[]{txtDescription, txtQtyOnHand, txtUnitPrice}) {
+                    txt.setDisable(false);
+                    txt.setEditable(false);
+                }
+                txtQty.setDisable(cur.getQty() == 0);
+            } else {
+                for (TextField txt : new TextField[]{txtDescription, txtQtyOnHand, txtUnitPrice, txtQty}) {
+                    txt.setDisable(true);
+                    txt.clear();
+                }
+            }
+        });
+        txtQty.textProperty().addListener((ov, prevQty, curQty) -> {
+            Item selectedItem = cmbItemCode.getSelectionModel().getSelectedItem();
+//            btnSave.setDisable(true);
+//            if (cur.matches("\\d+")){
+//                if (Integer.parseInt(cur) <= selectedItem.getQty() && Integer.parseInt(cur) > 0){
+//                    btnSave.setDisable(false);
+//                }
+//            }
+            btnSave.setDisable(!(curQty.matches("\\d+") && Integer.parseInt(curQty) <= selectedItem.getQty()
+                    && Integer.parseInt(curQty) > 0));
+        });
+    }
+    private void newOrder() throws IOException {
+        for (TextField txt : new TextField[]{txtCustomerName, txtDescription, txtQty, txtQtyOnHand, txtUnitPrice}) {
+            txt.clear();
+            txt.setDisable(true);
+        }
+    }
+
+    private void enablePlaceOrderButton(){
+        Customer selectedCustomer = cmbCustomerId.getSelectionModel().getSelectedItem();
+        btnPlaceOrder.setDisable(!(selectedCustomer != null && !tblOrderDetails.getItems().isEmpty()));
+    }
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
-        URL resource = this.getClass().getResource("/view/MainForm.fxml");
-        Parent root = FXMLLoader.load(resource);
-        Scene scene = new Scene(root);
-        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        Platform.runLater(primaryStage::sizeToScene);
+        MainFormController.navigateToMain(root);
     }
 
     public void btnAdd_OnAction(ActionEvent actionEvent) {
